98a1f79cacce9ff0a3959336aebcf8b0b1d502df
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-07-03 06:43:13 -0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  tvItems = ['allo', 'testin', 'rouge'];
+  contents: TVItem[] = [];
+
+  constructor(public httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.title = 'bonjour';
+    this.httpClient.get<TVItem[]>('api/tvitem?take=5&detail=r').subscribe(r => this.contents = r);
+  }
+}
+
+export interface TVItem {
+  TVItemID: number;
+  TVLevel: number;
+  TVPath: string;
+  TVType: number;
+  ParentID: number;
+  IsActive: boolean;
+  LastUpdateDate_UTC: Date;
+  LastUpdateContactTVItemID: number;
+  HasErrors: boolean;
+  ValidationResults: string[];
+  TVItemWeb: TVItemWeb;
+  TVItemReport: TVItemReport;
+}
+
+export interface TVItemWeb {
+  TVText: string;
+  LastUpdateContactTVText: string;
+  TVTypeText: string;
+}
+
+export interface TVItemReport {
+  TVItemReportTest: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
