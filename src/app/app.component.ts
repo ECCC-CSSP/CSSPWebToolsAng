82d17dd76a60f877f70376712215d6be67ae2315
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-08-15 15:22:17 -0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { TVItem } from './model/TVItem';
-import { QueryOptions } from './service/queryoptions';
-import { Observable } from 'rxjs';
-import { ApiService } from './service/api.service';
+// import { HttpClient } from '@angular/common/http';
+// import { TVItem } from './model/TVItem';
+// import { QueryOptions } from './service/queryoptions';
+// import { Observable } from 'rxjs';
+// import { ApiService } from './service/api.service';
+import { fromEvent } from 'rxjs';
+import { throttleTime, map, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,28 +13,38 @@ import { ApiService } from './service/api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  tvItems$: Observable<TVItem[]>;
-  tvItemsShortList$: Observable<TVItem[]>;
-  tvItem: TVItem;
-
-  constructor(private httpClient: HttpClient) {
+  constructor() {
   }
 
   ngOnInit() {
-    const apiService: ApiService<TVItem> = new ApiService<TVItem>(this.httpClient, 'api/tvitem');
-
-    this.tvItems$ = apiService.list(new QueryOptions('fr', 0, 140, '', 'TVText|EQ|Base', 'r'));
+    const button = document.querySelector('button');
+    fromEvent(button, 'click').pipe(
+    )
+      .subscribe(cl => console.log((<MouseEvent>cl).clientX));
   }
 
-  LoadShortList(): void {
-    const apiService2: ApiService<TVItem> = new ApiService(this.httpClient, 'api/tvitem');
+  // tvItems$: Observable<TVItem[]>;
+  // tvItemsShortList$: Observable<TVItem[]>;
+  // tvItem: TVItem;
 
-    this.tvItemsShortList$ = apiService2.list(new QueryOptions('fr', 3, 5, '', '', 'w'));
+  // constructor(private httpClient: HttpClient) {
+  // }
 
-    const apiService3: ApiService<TVItem> = new ApiService(this.httpClient, 'api/tvitem');
+  // ngOnInit() {
+  //   const apiService: ApiService<TVItem> = new ApiService<TVItem>(this.httpClient, 'api/tvitem');
 
-    apiService3.read(7, new QueryOptions('fr', 0, 100, '', '', 'w')).subscribe(r => this.tvItem = r);
+  //   this.tvItems$ = apiService.list(new QueryOptions('fr', 0, 140, '', 'TVText|EQ|Base', 'r'));
+  // }
 
-  }
+  // LoadShortList(): void {
+  //   const apiService2: ApiService<TVItem> = new ApiService(this.httpClient, 'api/tvitem');
+
+  //   this.tvItemsShortList$ = apiService2.list(new QueryOptions('fr', 3, 5, '', '', 'w'));
+
+  //   const apiService3: ApiService<TVItem> = new ApiService(this.httpClient, 'api/tvitem');
+
+  //   apiService3.read(7, new QueryOptions('fr', 0, 100, '', '', 'w')).subscribe(r => this.tvItem = r);
+
+  // }
 
 }
