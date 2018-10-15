d8d5ab56eb970ebff705baddff02053bb6cf9684
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-10-15 20:06:52 -0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { HttpClient } from '@angular/common/http';
-import { TVItem, TVItemA } from './model/TVItem';
 import { QueryOptions } from './service/queryoptions';
 import { Observable } from 'rxjs';
 import { ApiService } from './service/api.service';
+import { TVItemExtraB } from './interfaces/generated/tvitemextrab.interface';
 
 export interface Transaction {
   item: string;
@@ -19,28 +19,28 @@ export interface Transaction {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  tvItems$: Observable<TVItem[]>;
-  tvItemsShortList$: Observable<TVItem[]>;
-  tvItem: TVItem;
+  tvItems$: Observable<TVItemExtraB[]>;
+  tvItemsShortList$: Observable<TVItemExtraB[]>;
+  tvItem: TVItemExtraB;
   panelOpenState = false;
 
   constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit() {
-    const apiService: ApiService<TVItem> = new ApiService<TVItem>(this.httpClient, 'api/tvitem');
+    const apiService: ApiService<TVItemExtraB> = new ApiService<TVItemExtraB>(this.httpClient, 'api/tvitem');
 
-    this.tvItems$ = apiService.list(new QueryOptions('fr', 0, 140, '', '', ''));
+    this.tvItems$ = apiService.list(new QueryOptions('fr', 0, 14, '', '', 'ExtraB'));
   }
 
   LoadShortList(): void {
-    const apiService2: ApiService<TVItem> = new ApiService(this.httpClient, 'api/tvitem');
+    const apiService2: ApiService<TVItemExtraB> = new ApiService(this.httpClient, 'api/tvitem');
 
-    this.tvItemsShortList$ = apiService2.list(new QueryOptions('fr', 3, 5, '', '', ''));
+    this.tvItemsShortList$ = apiService2.list(new QueryOptions('fr', 3, 5, '', '', 'ExtraB'));
 
-    const apiService3: ApiService<TVItem> = new ApiService(this.httpClient, 'api/tvitem');
+    const apiService3: ApiService<TVItemExtraB> = new ApiService(this.httpClient, 'api/tvitem');
 
-    apiService3.read(7, new QueryOptions('fr', 0, 100, '', '', '')).subscribe(a => this.tvItem = a);
+    apiService3.read(7, new QueryOptions('fr', 0, 100, '', '', 'ExtraB')).subscribe(a => this.tvItem = a);
 
   }
 }
